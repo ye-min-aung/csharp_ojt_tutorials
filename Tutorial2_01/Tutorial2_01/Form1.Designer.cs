45f034ce73d44bce4ff3e9bdce8f69f65f24e9f8
ye-min-aung <yeminaung0901@gmail.com>
2024-09-23 17:01:38 +0630

    tutorial 2-01 commit

---
+++ Tutorial2_01/Tutorial2_01/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Tutorial2_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            txtName = new TextBox();
+            txtEmail = new TextBox();
+            txtPhone = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            cid = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            email = new DataGridViewTextBoxColumn();
+            phone = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(280, 46);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(280, 98);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(125, 27);
+            txtEmail.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(280, 151);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(125, 27);
+            txtPhone.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(125, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(125, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Email";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(125, 154);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Phone";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(464, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = Color.DodgerBlue;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { cid, name, email, phone });
+            dataGridView1.Location = new Point(125, 228);
+            dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.White;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(630, 188);
+            dataGridView1.TabIndex = 7;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // cid
+            // 
+            cid.HeaderText = "Card ID";
+            cid.MinimumWidth = 6;
+            cid.Name = "cid";
+            cid.Width = 125;
+            // 
+            // name
+            // 
+            name.HeaderText = "Member Name";
+            name.MinimumWidth = 6;
+            name.Name = "name";
+            name.Width = 200;
+            // 
+            // email
+            // 
+            email.HeaderText = "Email";
+            email.MinimumWidth = 6;
+            email.Name = "email";
+            email.Width = 125;
+            // 
+            // phone
+            // 
+            phone.HeaderText = "Phone No.";
+            phone.MinimumWidth = 6;
+            phone.Name = "phone";
+            phone.Width = 125;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(929, 520);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPhone);
+            Controls.Add(txtEmail);
+            Controls.Add(txtName);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtName;
+        private TextBox txtEmail;
+        private TextBox txtPhone;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn cid;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn email;
+        private DataGridViewTextBoxColumn phone;
+    }
+}
---
+++ Tutorial2_01/Tutorial2_01/Form1.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Tutorial2_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cid.DataPropertyName = "cid";
+            name.DataPropertyName = "name";
+            email.DataPropertyName = "email";
+            phone.DataPropertyName = "phone";
+
+        }
+
+        int id = 1;
+        List<Member> list = new List<Member>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dataGridView1.AutoGenerateColumns = false;
+            string name = txtName.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+
+            if (email.Contains("@") && email.Contains("."))
+            {
+
+                if (!IsPhone(phone))
+                {
+                    MessageBox.Show("invalid phone");
+                }
+
+                else
+                {
+                    string data = id + " ,MC-00" + id + " ," + email + " ," + phone;
+
+                    Member mem = new Member();
+                    mem.cid = "MC-000" + id;
+                    mem.name = name;
+                    mem.email = email;
+                    mem.phone = phone;
+                    
+                    list.Add(mem);
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = list;
+                    id++;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("invalid email");
+            }
+
+            txtName.Text = null;
+            txtEmail.Text = null;
+            txtPhone.Text = null;
+
+        }
+
+        bool IsPhone(string phone)
+        {
+            foreach (char c in phone)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Member
+    {
+        public string cid { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }   
+          
+    }
+
+}
